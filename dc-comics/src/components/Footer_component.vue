b2c810c16846d7227425578f5a1a9f8b895fc021
MyUsername <romamargine@gmail.com>
2022-05-03 15:30:49 +0200

    Initial commit

---
+++ dc-comics/src/components/Footer_component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<style lang="scss">
+
+</style>
---
+++ dc-comics/src/components/Header_component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<style lang="scss">
+
+</style>
